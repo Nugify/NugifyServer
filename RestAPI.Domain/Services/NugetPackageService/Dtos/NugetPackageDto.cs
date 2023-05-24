641ab745411929f56bae14fd0a65108ff61dd44c
fleimeris <44847763+fleimeris@users.noreply.github.com>
2023-05-24 22:55:05 +0300

    fixed xml parser

--- RestAPI.Domain/Services/NugetPackageService/Dtos/NugetPackageDto.cs
+++ RestAPI.Domain/Services/NugetPackageService/Dtos/NugetPackageDto.cs
@@ -2,9 +2,8 @@ using System.Xml.Serialization;
 
 namespace RestAPI.Domain.Services.NugetPackageService.Dtos;
 
-[XmlRoot(ElementName = "package", Namespace = "http://schemas.microsoft.com/packaging/2010/07/nuspec.xsd")]
+[Serializable, XmlRoot("package", Namespace = "http://schemas.microsoft.com/packaging/2013/05/nuspec.xsd", IsNullable = true)]
 public class NugetPackageDto
 {
-    [XmlElement(ElementName = "metadata")]
-    public NugetPackageMetadataDto? Metadata { get; set; }
+    [XmlElement(ElementName = "metadata")] public NugetPackageMetadataDto? Metadata { get; set; }
 }
--- RestAPI.Domain/Services/NugetPackageService/NugetPackageService.cs
+++ RestAPI.Domain/Services/NugetPackageService/NugetPackageService.cs
@@ -7,32 +7,24 @@ namespace RestAPI.Domain.Services.NugetPackageService;
 
 public class NugetPackageService : INugetPackageService
 {
-    public async Task<NugetValidationResultDto> ValidateNugetPackage(MemoryStream nugetPackageStream, CancellationToken cancellationToken)
+    public async Task<NugetValidationResultDto> ValidateNugetPackage(MemoryStream nugetPackageStream,
+        CancellationToken cancellationToken)
     {
         using var zip = new ZipFile(nugetPackageStream);
 
         foreach (ZipEntry entry in zip)
         {
-            if(!entry.IsFile)
+            if (!entry.IsFile)
                 continue;
-            
-            if(!entry.Name.EndsWith(".nuspec", StringComparison.OrdinalIgnoreCase))
+
+            if (!entry.Name.EndsWith(".nuspec", StringComparison.OrdinalIgnoreCase))
                 continue;
 
             using var entryReader = new StreamReader(zip.GetInputStream(entry));
 
-            var xmlDoc = new XmlDocument();
-            var readToEndAsync = await entryReader.ReadToEndAsync(cancellationToken);
-            xmlDoc.LoadXml(readToEndAsync);
-
-            var settings = new XmlReaderSettings
-            {
-                IgnoreWhitespace = true
-            };
-
-            using var reader = XmlReader.Create(new XmlNodeReader(xmlDoc), settings);
+            var xmlReader = new XmlTextReader(entryReader);
             var serializer = new XmlSerializer(typeof(NugetPackageDto));
-            var nugetPackage = (NugetPackageDto)serializer.Deserialize(reader);
+            var nugetPackage = serializer.Deserialize(xmlReader) as NugetPackageDto;
 
             if (nugetPackage == null || nugetPackage.Metadata == null)
                 return new NugetValidationResultDto
@@ -47,7 +39,7 @@ public class NugetPackageService : INugetPackageService
                 PackageMetadata = nugetPackage.Metadata
             };
         }
-        
+
         return new NugetValidationResultDto
         {
             IsValid = false,
